b13a022367cf7620c1ccec6f74622b3d63e19c92
Erick Birbe <erickcion@gmail.com>
2012-10-01 17:14:51 -0430

    Agregando licencia a archivos

--- canaimainstalador/clases/particion_redimensionar.py
+++ canaimainstalador/clases/particion_redimensionar.py
@@ -1,9 +1,28 @@
-#-*- coding: UTF-8 -*-
-'''
-Created on 13/09/2012
+# -*- coding: utf-8 -*-
+#
+# ==============================================================================
+# PAQUETE: canaima-instalador
+# ARCHIVO: canaimainstalador/translator.py
+# COPYRIGHT:
+#       (C) 2012 William Abrahan Cabrera Reyes <william@linux.es>
+#       (C) 2012 Erick Manuel Birbe Salazar <erickcion@gmail.com>
+#       (C) 2012 Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
+# LICENCIA: GPL-3
+# ==============================================================================
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# COPYING file for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-@author: Erick Birbe <erickcion@gmail.com>
-'''
 import gtk
 from canaimainstalador.clases.common import humanize, TblCol, floatify, PStatus, \
     SECTOR
