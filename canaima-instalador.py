dc1fcd906750c7b227c98b121e05991c8cb302a5
Orlando Fiol <fiolorlando@gmail.com>
2013-10-11 18:02:46 -0430

    Migrado mensaje de error de permiso

--- canaima-instalador.py
+++ canaima-instalador.py
@@ -31,7 +31,9 @@ from canaimainstalador.config import CFG, BANNER_IMAGE
 from canaimainstalador.main import Bienvenida, Wizard
 from canaimainstalador.translator import MAIN_ROOT_ERROR_MSG, \
     MAIN_ROOT_ERROR_TITLE, gettext_install
-import gtk
+#import gtk
+from gi.repository import GObject
+from gi.repository import Gtk as gtk
 import os
 import sys
 
@@ -42,8 +44,8 @@ if __name__ == "__main__":
     if os.geteuid() != 0:
         dialog = UserMessage(
             message=MAIN_ROOT_ERROR_MSG, title=MAIN_ROOT_ERROR_TITLE,
-            mtype=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK,
-            c_1=gtk.RESPONSE_OK, f_1=sys.exit, p_1=(1,)
+            mtype=gtk.MessageType.ERROR, buttons=gtk.ButtonsType.OK,
+            c_1=gtk.ResponseType.OK, f_1=sys.exit, p_1=(1,)
             )
     else:
         CFG['w'] = Wizard(700, 470, _('Canaima Installation'), BANNER_IMAGE)
